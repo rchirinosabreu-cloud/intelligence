f39aa7c61ae692e907cfd6514336c082daf46e40
rchirinosabreu-cloud <rchirinosabreu@gmail.com>
2026-01-21 16:18:33 -0500

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,6 @@
-export const cn = (...classes) => classes.filter(Boolean).join(" ");
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs) {
+	return twMerge(clsx(inputs));
+}
