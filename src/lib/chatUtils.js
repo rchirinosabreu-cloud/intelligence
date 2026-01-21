feb56591d4d32f05818f96ee56b84433900afa05
rchirinosabreu-cloud <rchirinosabreu@gmail.com>
2026-01-21 16:17:58 -0500

    Update chatUtils.js

--- src/lib/chatUtils.js
+++ src/lib/chatUtils.js
@@ -1,12 +1,6 @@
-export const formatTimestamp = (date) =>
-  new Intl.DateTimeFormat("es-ES", {
-    hour: "2-digit",
-    minute: "2-digit",
-  }).format(date);
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-export const buildMessage = (role, content) => ({
-  id: `${role}-${Date.now()}-${Math.random().toString(16).slice(2)}`,
-  role,
-  content,
-  timestamp: formatTimestamp(new Date()),
-});
+export function cn(...inputs) {
+	return twMerge(clsx(inputs));
+}
