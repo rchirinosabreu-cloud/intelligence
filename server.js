ba27928192406e291d225cd7e7d0a43ae35d2cc6
rchirinosabreu-cloud <rchirinosabreu@gmail.com>
2026-01-24 02:07:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // --- AUTHENTICATION SETUP ---
 let credentials;
