e58b9b05069d2c490f9930969074ef6a1c801c0e
brainstudio-hub <coordinadorbrainstudio@gmail.com>
2026-02-26 18:38:50 -0500

    Update prisma.js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,18 +1,14 @@
-import { PrismaClient } from '@prisma/client';
-
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
-};
+// Importamos como paquete de CommonJS para evitar el bug de Node 22
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 const globalForPrisma = globalThis;
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+// Ahora sí, instanciamos la clase correcta con los paréntesis vacíos
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
